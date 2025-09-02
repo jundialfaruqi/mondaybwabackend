f33794a748635473ebdba2eb0ab29c0045bcb885
jundialfaruqi <jundialfaruqi@gmail.com>
2025-09-02 10:28:46 +0700

    Model & Relasi

--- app/Models/Merchant.php
+++ app/Models/Merchant.php
@@ -3,8 +3,41 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Merchant extends Model
 {
-    //
+    //Digunakan untuk memungkinkan fitur soft delete pada model ini. Dengan menggunakan SoftDeletes, data tidak akan dihapus secara permanen dari database, melainkan akan ditandai sebagai dihapus dengan kolom deleted_at. Ini memungkinkan kamu untuk "mengembalikan" data yang terhapus jika dibutuhkan.
+    use SoftDeletes;
+
+    //fillable adalah properti yang digunakan untuk menentukan atribut mana yang dapat diisi secara massal (mass assignable). Dengan mendefinisikan atribut-atribut ini dalam $fillable, kamu dapat melindungi model dari serangan mass assignment, di mana pengguna jahat dapat mencoba mengisi atribut yang tidak seharusnya mereka miliki aksesnya.
+    protected $fillable = ['name', 'address', 'photo', 'phone', 'keeper_id'];
+
+    //belongsTo digunakan untuk mendefinisikan relasi one-to-many atau inverse relationship. Dalam kasus ini, Merchant belongs to (milik) satu User yang diidentifikasi oleh kolom keeper_id. Relasi ini berarti setiap Merchant terkait dengan satu User yang memiliki peran sebagai keeper (penjaga atau pemilik). Biasanya, keeper_id akan berisi ID dari pengguna yang bertanggung jawab atas merchant ini.
+    public function keeper()
+    {
+        return $this->belongsTo(User::class, 'keeper_id');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'merchant_products')
+            ->withPivot('stock')
+            ->withPivot('warehouse_id')
+            ->withTimestamps();
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function getPhotoAttribute($value)
+    {
+        if (!$value) {
+            return null;
+        }
+        return url(Storage::url($value));
+    }
 }
--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Warehouse extends Model
 {
@@ -27,6 +28,6 @@ class Warehouse extends Model
         if (!$value) {
             return null;
         }
-        return url(\Illuminate\Support\Facades\Storage::url($value));
+        return url(Storage::url($value));
     }
 }
